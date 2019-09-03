8ed0b6f0d31b1744aaa16ac6831be26a6a47b168
Leigh Barnes <leighbrenmdonbarnes@gmail.com>
2019-09-03 23:46:14 +0100

    Add CircleCi, Deployer, Workflows
    Adds a custom deployer to work with a circle config in an error handled
    manner, changed from credentials to environment vars so as not to have
    to commit details.
    
    Does not use Grunt in this implmentation

---
+++ circle_scripts/checkThemeIsReady.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const axios = require('axios')
+const themeAudit = require(path.resolve('./public/theme.json'))
+
+const checkThemeIsReady = async () => {
+
+  const currentStatus = await axios.get(themeAudit.themeCheckUrl)
+  
+  if(!currentStatus.data || !currentStatus.data.theme) {
+    throw new Error('Could not retrieve theme') 
+  }
+
+  if(!currentStatus.data.theme.processing && currentStatus.data.theme.previewable) {
+    console.log('theme is ready to go, fire up cypress!')
+    return true
+  }
+
+  console.log('Theme is not ready retrying in ten seconds ...')
+  return setTimeout(checkThemeIsReady, 10000)
+
+}
+
+checkThemeIsReady();

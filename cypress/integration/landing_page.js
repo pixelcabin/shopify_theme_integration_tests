2ace9c0ee8efd9aee5cc48d01ad57dd59cb1ea67
Kenrick <kenrickyuan@gmail.com>
2021-02-24 22:41:33 +0100

    chore: Add comment on landing_page.js about inputting the dev store password beforeEach test

--- cypress/integration/landing_page.js
+++ cypress/integration/landing_page.js
@@ -1,6 +1,9 @@
 describe('Landing Page', function() {
   beforeEach(function() {
     cy.visit('/');
+    // Most likely the dev site you are testing on is password protected, so you can do something like below to enter the password every test
+    // cy.get('#Password').type('password_here');
+    // cy.get('#login_form .input-group .input-group__btn > button').click();
   });
 
   it('Wholesaler section should be scrolled to from link in menu', function() {
